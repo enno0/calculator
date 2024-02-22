6f0ceee1243126b12e934d1674f237cadfd567f3
Kamal_Mohamed <kamalaldeenmohamed@gmail.com>
2024-02-22 17:26:45 +0200

    up

---
+++ enno/src/test/java/com/calculator/enno/EnnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.enno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
